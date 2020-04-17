481c8531cb9d507edfe8acd9c0b96d058b4d8b3f
mat <matan.selig.ubiqum@gmail.com>
2020-04-17 14:51:32 +0200

    start backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(
@@ -9,8 +12,6 @@ app.use(
 );
 app.use(cors());
 
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + " port");
