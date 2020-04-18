0c303b3a0c30c44f73fdc67f10da3c89eacd25d6
mat <matan.selig.ubiqum@gmail.com>
2020-04-18 18:55:09 +0200

    mongoDb conction epic 3 done

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://user1:1234@travelappcluster-e7tsh.mongodb.net/TravelAppCluster?retryWrites=true&w=majority",
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./keys").mongoURI;
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 app.use(
@@ -8,10 +18,7 @@ app.use(
   })
 );
 app.use(cors());
+app.use("/cities", require("./routes/cities"));
 
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server is running on " + port + " port");
-});
