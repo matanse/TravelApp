93c19d3f4748256cc8216f4ab8fc685efa568f0a
mat <matan.selig.ubiqum@gmail.com>
2020-04-23 12:05:41 +0200

    this is how it was

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -8,50 +8,60 @@ import CitiesThird from "./CitiesThird";
 import "./css/cities.css";
 
 export default class Cities extends Component {
-  state = { cities: [], firstCities: [] };
+  state = {};
+
+  // divideCities = (start, end) => {
+  //   let list = [];
+  //   for (let x = 0; start <= x <= end; x++) {
+  //     list = [...list, cities[x]];
+  //   }
+  // };
 
   fetchCities = () => {
-    axios.get("http://localhost:5000/cities/all").then((res) => {
-      this.setState({ cities: res.data });
-      this.setState({ firstCities: this.divideCities(0, 3) });
-      console.log(this.state.cities);
-    });
-  };
-
-  divideCities = (start, end) => {
-    let list = [];
-    for (let x = 0; start <= x <= end; x++) {
-      list = [...list, this.state.cities[x]];
-    }
-    console.log(list);
-    return list;
+    axios
+      .get("http://localhost:5000/cities/all")
+      .then((res) => this.setState({ cities: res.data }));
   };
 
   componentDidMount() {
     this.fetchCities();
-    // this.setState({ firstCities: this.divideCities(0, 3) });
   }
 
   render() {
-    // const cities = this.state.cities;
-    // const first4Cities = [cities[0], cities[1], cities[2], cities[3]];
-    // const second4Cities = [cities[4], cities[5], cities[6], cities[7]];
-    // const third4Cities = [cities[8], cities[9], cities[10], cities[11]];
-    console.log(this.state.cities);
-    return this.state.cities !== [] ? (
-      <div style={{ textAlign: "center" }}>
-        <h3>Popular MYtineraries</h3>
-        <Carousel>
-          <div>
-            {/* {" "}
-            {this.state.firstCities !== [] && (
-              // <CitiesFirst cities={this.state.firstCities} />
-            )}{" "} */}
-          </div>
-          <div>{/* <CitiesSecond cities={second4Cities} /> */}</div>
-          <div>{/* <CitiesThird cities={third4Cities} /> */}</div>
-        </Carousel>
-      </div>
-    ) : null;
+    if (this.state.cities === undefined) {
+      return (
+        <div style={{ textAlign: "center" }}>
+          <h1>Loding...</h1>
+        </div>
+      );
+    } else {
+      // console.log(this.state.cities);
+      const cities = this.state.cities;
+      const first4Cities = [cities[0], cities[1], cities[2], cities[3]];
+      const second4Cities = [cities[4], cities[5], cities[6], cities[7]];
+      const third4Cities = [cities[8], cities[9], cities[10], cities[11]];
+      return (
+        <div style={{ textAlign: "center" }}>
+          <h3>Popular MYtineraries</h3>
+          <Carousel>
+            <div>
+              <CitiesFirst cities={first4Cities} />
+            </div>
+            <div>
+              <CitiesSecond cities={second4Cities} />
+            </div>
+            <div>
+              <CitiesThird cities={third4Cities} />
+            </div>
+          </Carousel>
+        </div>
+      );
+    }
+
+    // const
+    // divideCities(first4Cities, 0, 4);
+    // divideCities(second4Cities, 5, 8);
+    // divideCities(third4Cities, 9, 12);
+    // console.log(second4Cities);
   }
 }
