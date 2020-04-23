d03e5835bc64bd7ffd6da6f0d8094c7ee1ac70f6
mat <matan.selig.ubiqum@gmail.com>
2020-04-23 13:26:51 +0200

    data is connected cities carousel works

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -2,25 +2,24 @@ import React, { Component } from "react";
 import axios from "axios";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import CitiesFirst from "./CitiesFirst";
-import CitiesSecond from "./CitiesSecond";
-import CitiesThird from "./CitiesThird";
+import CitiesCarousel from "./CitiesCarousel";
 import "./css/cities.css";
 
 export default class Cities extends Component {
-  state = {};
+  state = { cities: [], loading: false };
 
-  // divideCities = (start, end) => {
-  //   let list = [];
-  //   for (let x = 0; start <= x <= end; x++) {
-  //     list = [...list, cities[x]];
-  //   }
-  // };
+  divideCities = (start, end) => {
+    let list = [];
+    for (let x = start; x <= end; x++) {
+      list = [...list, this.state.cities[x]];
+    }
+    return list;
+  };
 
   fetchCities = () => {
     axios
       .get("http://localhost:5000/cities/all")
-      .then((res) => this.setState({ cities: res.data }));
+      .then((res) => this.setState({ cities: res.data, loading: true }));
   };
 
   componentDidMount() {
@@ -28,40 +27,27 @@ export default class Cities extends Component {
   }
 
   render() {
-    if (this.state.cities === undefined) {
-      return (
-        <div style={{ textAlign: "center" }}>
-          <h1>Loding...</h1>
-        </div>
-      );
-    } else {
-      // console.log(this.state.cities);
-      const cities = this.state.cities;
-      const first4Cities = [cities[0], cities[1], cities[2], cities[3]];
-      const second4Cities = [cities[4], cities[5], cities[6], cities[7]];
-      const third4Cities = [cities[8], cities[9], cities[10], cities[11]];
-      return (
-        <div style={{ textAlign: "center" }}>
-          <h3>Popular MYtineraries</h3>
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h3>Popular MYtineraries</h3>
+        {this.state.loading ? (
           <Carousel>
             <div>
-              <CitiesFirst cities={first4Cities} />
+              <CitiesCarousel cities={this.divideCities(0, 3)} />
             </div>
             <div>
-              <CitiesSecond cities={second4Cities} />
+              <CitiesCarousel cities={this.divideCities(4, 7)} />
             </div>
             <div>
-              <CitiesThird cities={third4Cities} />
+              <CitiesCarousel cities={this.divideCities(8, 11)} />
             </div>
           </Carousel>
-        </div>
-      );
-    }
-
-    // const
-    // divideCities(first4Cities, 0, 4);
-    // divideCities(second4Cities, 5, 8);
-    // divideCities(third4Cities, 9, 12);
-    // console.log(second4Cities);
+        ) : (
+          <div style={{ textAlign: "center" }}>
+            <h1>Loading...</h1>
+          </div>
+        )}
+      </div>
+    );
   }
 }
